12ded594e49f9cb96c0cef38bc52477b21fbf864
devops <35161605+devplusopsautomation@users.noreply.github.com>
2024-11-29 16:38:16 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
   alias  = "prod"
+  access_key = "AKIA3FLDZZRK35BQH36R"
+  secret_key = "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"
 }
 
 provider "aws" {
   region = "us-east-1"
   alias  = "nonprod"
+  access_key = "AKIA3FLDZZRK35BQH36R"
+  secret_key = "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"
 }
